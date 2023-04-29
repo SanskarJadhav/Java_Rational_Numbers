054232cacdd2c00d7fa232d237aa633ae8f8f9c0
Sanskar Jadhav <113002227+SanskarJadhav@users.noreply.github.com>
2023-04-29 19:56:19 +0530

    Update ZeroDenominatorException.java

--- ZeroDenominatorException.java
+++ ZeroDenominatorException.java
@@ -1,3 +1,4 @@
+// creating a custom exception for if denominator is entered as 0
 class ZeroDenominatorException extends Exception {
     public ZeroDenominatorException() {
         super("Denominator cannot be zero");
